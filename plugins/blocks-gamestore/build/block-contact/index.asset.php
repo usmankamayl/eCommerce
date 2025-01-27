805b91e71e3b3e07cf4c7d372947a90480d23c5b
usman <usman@MacBook-Air-usman.local>
2025-01-27 13:33:58 +0300

    first commit

---
+++ plugins/blocks-gamestore/build/block-contact/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '623890c84d0048d2ca07');
---
+++ plugins/blocks-gamestore/build/block-hero/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'ebe45e11893cbf83e829');
